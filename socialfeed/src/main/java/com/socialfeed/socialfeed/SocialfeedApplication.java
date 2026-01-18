781b05707c3f73b30c92a81f1744f53b54fa4e55
배성준 <john41011@bu.ac.kr>
2026-01-18 19:32:25 +0900

    Initial commit: Spring Boot project setup

---
+++ socialfeed/src/main/java/com/socialfeed/socialfeed/SocialfeedApplication.java
@@ -0,0 +1,13 @@
+package com.socialfeed.socialfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialfeedApplication.class, args);
+	}
+
+}
